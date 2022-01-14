93e888b5f5cbeb3101e9ebb20ff15f378008d94c
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-01-13 17:38:11 -0800

    fixed last commit

--- api/server.js
+++ api/server.js
@@ -5,4 +5,10 @@ const userRouter = require('./users/users-router.js');
 server.use(express.json());
 server.use('/api', userRouter);
 
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Node Module Project 4</h1>
+    `)
+})
+
 module.exports = server;
